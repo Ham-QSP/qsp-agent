8f870002ba2715b173052526d77a1d9cb26a2a57
Florian <76635263+f4fez@users.noreply.github.com>
2026-06-28 17:19:38 +0200

    Fix release

--- lib-hamlib/build.rs
+++ lib-hamlib/build.rs
@@ -2,30 +2,25 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to look for shared libraries in the specified directory
-    println!("cargo:rustc-link-search=/opt/homebrew/lib");
+    let mut builder = bindgen::Builder::default().header("wrapper.h");
 
-    // Tell cargo to tell rustc to link hamlib
-    // shared library.
-    println!("cargo:rustc-link-lib=hamlib");
+    if let Ok(library) = pkg_config::Config::new().probe("hamlib") {
+        for include_path in library.include_paths {
+            builder = builder.clang_arg(format!("-I{}", include_path.display()));
+        }
+    } else if cfg!(target_os = "macos") {
+        println!("cargo:rustc-link-search=/opt/homebrew/lib");
+        println!("cargo:rustc-link-lib=hamlib");
+        builder = builder.clang_arg("-I/opt/homebrew/include");
+    } else {
+        println!("cargo:rustc-link-lib=hamlib");
+    }
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header("wrapper.h")
-        .clang_arg("-I/opt/homebrew/include")
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
+    let bindings = builder
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
