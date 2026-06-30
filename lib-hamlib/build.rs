6f5faa76ce0c828b5040037188578477d9c6825a
Florian <76635263+f4fez@users.noreply.github.com>
2026-06-30 22:42:00 +0200

    Fix debian build

--- lib-hamlib/build.rs
+++ lib-hamlib/build.rs
@@ -32,19 +32,21 @@ fn main() {
 
     let bindings_source = fs::read_to_string(&bindings_path).expect("Couldn't read bindings");
     if let Some(vprintf_cb_pos) = bindings_source.find("pub type vprintf_cb_t") {
-        if let Some(vprintf_cb_end) = bindings_source[vprintf_cb_pos..].find(">;") {
-            let end = vprintf_cb_pos + vprintf_cb_end;
-            let vprintf_cb_section = &bindings_source[vprintf_cb_pos..end];
-            let normalized: String = vprintf_cb_section
-                .chars()
-                .filter(|ch| !ch.is_whitespace())
-                .collect();
+        let vprintf_cb_source = &bindings_source[vprintf_cb_pos..];
+        let vprintf_cb_end = vprintf_cb_source
+            .find("pub type freq_cb_t")
+            .or_else(|| vprintf_cb_source.find("pub type chan_cb_t"))
+            .unwrap_or_else(|| vprintf_cb_source.len().min(2_000));
+        let vprintf_cb_section = &vprintf_cb_source[..vprintf_cb_end];
+        let normalized: String = vprintf_cb_section
+            .chars()
+            .filter(|ch| !ch.is_whitespace())
+            .collect();
 
-            if normalized.contains("arg4:*mut__va_list_tag") {
-                println!("cargo:rustc-cfg=hamlib_vprintf_cb_uses_va_list_pointer");
-            }
-        } else {
-            println!("cargo:warning=Unable to detect vprintf_cb_t definition terminator");
+        if normalized.contains("arg4:*mut__va_list_tag")
+            || normalized.contains("fn(arg1:rig_debug_level_e,arg2:*mut::std::os::raw::c_void,arg3:*const::std::os::raw::c_char,arg4:*mut__va_list_tag)")
+        {
+            println!("cargo:rustc-cfg=hamlib_vprintf_cb_uses_va_list_pointer");
         }
     } else {
         println!("cargo:warning=Unable to locate vprintf_cb_t definition in generated bindings");
